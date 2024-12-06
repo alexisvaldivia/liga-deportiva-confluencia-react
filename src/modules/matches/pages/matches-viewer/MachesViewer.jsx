4de45e95d97f3e45bb12189dbb62545591c04563
Alexis Valdivia <alesixvaldivia@gmail.com>
2024-12-06 12:15:08 -0300

    Componente para ver los partidos creado

---
+++ src/modules/matches/pages/matches-viewer/MachesViewer.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { db } from "../../../../firebase/firebase-config";
+import { getDocs, collection } from "firebase/firestore";
+
+export default function MachesViewer() {
+
+  useEffect((
+
+  ) => { }, [])
+
+  return (
+    <section>
+      <h2>Próximos Partidos</h2>
+    </section>
+  )
+}
